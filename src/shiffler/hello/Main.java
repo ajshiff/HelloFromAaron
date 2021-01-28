aa98452a521cd01f86c5a7f85a80dc52cba58b95
Aaron Shiffler <ajshiffler@gmail.com>
2021-01-28 11:51:31 -0700

    Initial commit

---
+++ src/shiffler/hello/Main.java
@@ -0,0 +1,13 @@
+package shiffler.hello;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Team Member Names
+        System.out.println("Hello, my name is Aaron");
+    }
+}
