eaa308c3f2afc4551545a3ec53347c0c42c3a767
Michael Cabrera <michael.cabrera@vivint.com>
2021-01-28 15:54:27 -0700

    adding a line to tell Aaron my name

--- src/shiffler/hello/Main.java
+++ src/shiffler/hello/Main.java
@@ -9,5 +9,6 @@ public class Main {
     public static void main(String[] args) {
         // Team Member Names
         System.out.println("Hello, my name is Aaron");
+        System.out.println("Hello Aaron, my name is Mike");
     }
 }
